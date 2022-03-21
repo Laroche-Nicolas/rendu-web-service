dc686b01641c63678b749490782fa07b409efd14
Laroche-Nicolas <larochenicolas.info@gmail.com>
2022-03-21 21:21:16 +0100

    adding method for login and return an JWT

--- src/services/authentication.service.ts
+++ src/services/authentication.service.ts
@@ -1,7 +1,15 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { User } from '../models/user.model';
+import * as bCrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 import { UserRepository } from '../repositories/user.repository';
 import { UserDto } from '../dto/user.dto';
+import config from '../config';
+import { LoginDTO } from '../dto/login.dto';
 
 @Injectable()
 export class AuthenticationService {
@@ -12,19 +20,55 @@ export class AuthenticationService {
     const existingUser = await this.findUserByMail(email);
 
     if (!!existingUser) {
-      throw new BadRequestException();
+      throw new UnauthorizedException();
     }
 
     const user = await this.userRepository.insert({
       email,
-      password,
       firstname,
       lastname,
+      isActive: true,
     });
 
-    return this.userRepository.findOneBy(user._id);
+    user.password = await this.encryptPassword(password);
+
+    return user.save();
   };
 
-  findUserByMail = (mail: string): Promise<User> =>
-    this.userRepository.findOneBy({ email: mail });
+  login = async (parameters: LoginDTO): Promise<{ token: string }> => {
+    const user = await this.userRepository.findOneBy(
+      { email: parameters.email },
+      { hiddenPropertiesToSelect: ['password'] },
+    );
+
+    const passwordIsCorrect = await this.comparePassword({
+      password1: parameters.password,
+      password2: user?.password ?? '',
+    });
+
+    if (!passwordIsCorrect) {
+      throw new UnauthorizedException();
+    }
+
+    return {
+      token: this.createJwtToken(user),
+      // refreshToken: await this.createRefreshToken(user),
+    };
+  };
+
+  private findUserByMail = (email: string): Promise<User> =>
+    this.userRepository.findOneBy({ email: email });
+
+  private comparePassword = async ({
+    password1,
+    password2,
+  }): Promise<boolean> => bCrypt.compare(password1, password2);
+
+  private createJwtToken = (user: User): string =>
+    jwt.sign({ _id: user._id }, config.jwt.secretKey, {
+      expiresIn: config.jwt.expirationTime,
+    });
+
+  private encryptPassword = async (password: string): Promise<string> =>
+    bCrypt.hash(password, 15);
 }
