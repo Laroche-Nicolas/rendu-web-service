c5d3e24ae92808910d2d77fecc2cbf71e46de8d5
Laroche-Nicolas <larochenicolas.info@gmail.com>
2022-03-22 18:47:45 +0100

    Add an guard to verify if an user have a valid JWT

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JWTGuard } from '../guards/jwt.guard';
 import { AppService } from '../services/app.service';
+import { ApiSecurity } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseGuards(JWTGuard)
+  @ApiSecurity('Bearer')
   getHello(): string {
     return this.appService.getHello();
   }
