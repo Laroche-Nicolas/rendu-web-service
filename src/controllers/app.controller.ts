558152bd3c8d1d62409b862da18dde63c5d308a9
Laroche-Nicolas <larochenicolas.info@gmail.com>
2022-03-24 13:43:42 +0100

    delet unuse route

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,16 +1,7 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { JWTGuard } from '../guards/jwt.guard';
+import { Controller } from '@nestjs/common';
 import { AppService } from '../services/app.service';
-import { ApiSecurity } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  @UseGuards(JWTGuard)
-  @ApiSecurity('Bearer')
-  getHello(): string {
-    return this.appService.getHello();
-  }
 }
